7de70fc3a7ee85b66c68f1e218c8c4ae03660ab7
Juliana Câmara <julianabdcamara@gmail.com>
2025-12-10 09:55:01 -0300

    Create 01_select.sql

---
+++ 01_select.sql
@@ -0,0 +1,8 @@
+SELECT idCliente, qtdePontos, DtCriacao 
+FROM clientes;
+
+
+--- dá para fazer por blocos. só selecionar o que quer > clicar ao lado direito do mouse 
+SELECT *
+FROM clientes;
+LIMIT 10;
