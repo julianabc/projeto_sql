86461c49f38fda82e3de53b9674b939c1bad1905
Juliana Câmara <julianabdcamara@gmail.com>
2025-12-10 09:59:07 -0300

    Create 06_join.sql

---
+++ lista_exercicios/join/06_join.sql
@@ -0,0 +1,14 @@
+/* Qual mês tivemos mais lista de presença assinada? */
+
+SELECT substr(t1.DtCriacao, 1, 7) AS Mes, count(DISTINCT t1.IdTransacao) as Qtde
+FROM transacoes AS t1
+
+LEFT JOIN transacao_produto AS t2 ON t1.IdTransacao=t2.IdTransacao
+
+LEFT JOIN produtos AS t3 ON t2.IdProduto=t3.IdProduto
+
+WHERE t3.DescNomeProduto="Lista de presença"
+
+GROUP BY Mes
+
+ORDER BY Qtde DESC
