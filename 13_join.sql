2148df8c87e4c682facc1bbe3c0ae9e1a5837089
Juliana Câmara <julianabdcamara@gmail.com>
2025-12-10 09:56:58 -0300

    Create 13_join.sql

---
+++ 13_join.sql
@@ -0,0 +1,13 @@
+SELECT *
+FROM transacao_produto AS tp
+LEFT JOIN produtos AS p
+ON tp.IdProduto = p.IdProduto
+
+
+-- Para pegar todas da primeira tabela e só uma da segunda
+
+SELECT tp.*,
+       p.DescNomeProduto
+FROM transacao_produto AS tp
+LEFT JOIN produtos AS p
+ON tp.IdProduto = p.IdProduto
